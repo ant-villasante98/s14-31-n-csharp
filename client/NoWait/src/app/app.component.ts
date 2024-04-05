e4ce0c601e421f42637321b6ed9f1065a494f3ee
Prince Rolando Moreno Rodriguez <prins_ghost_2018@outlook.com>
2024-04-05 01:28:56 -0500

    Falta el menu hamburguesa

--- client/NoWait/src/app/app.component.ts
+++ client/NoWait/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
+import { NavbarComponent } from './shared/components/sctruct/navbar/navbar.component';
+import { BannerHomeComponent } from './shared/components/banners/banner-home/banner-home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, BannerHomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
