57dbdb66c7a80e10da69cf8f8fd9e84f6ecddae8
Villasante Hilares Heli Antonio <hilares33v@gmail.com>
2024-04-14 12:00:56 -0300

    intregacion con la api

--- client/NoWait/src/app/shared/services/env.service.ts
+++ client/NoWait/src/app/shared/services/env.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EnvService {
-  readonly HOST_API: string = "http://localhost:5065";
+  readonly HOST_API: string = "http://s14.runasp.net";
   readonly API_URL: string = `${this.HOST_API}/api`
   constructor() { }
 }
