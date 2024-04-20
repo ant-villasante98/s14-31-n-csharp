5933f173f6ebfde3b454b811826609d644baeff2
Villasante Hilares Heli Antonio <hilares33v@gmail.com>
2024-04-19 21:10:36 -0300

    detalles de carga de locales

--- client/NoWait/src/app/pages/locals/shop.component.ts
+++ client/NoWait/src/app/pages/locals/shop.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { MallsService } from '../../shared/services/malls.service';
 import { CardShopComponent } from './card-shop/card-shop.component';
 import { ResposeSearchFood } from '../../models/search-result';
@@ -10,19 +10,29 @@ import { ResposeSearchFood } from '../../models/search-result';
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css'
 })
-export class ShopComponent implements OnInit{
-  
+export class ShopComponent implements OnInit {
+
   private _mallsService = inject(MallsService);
   listShops!: ResposeSearchFood[];
+
+  finishSearch = signal<boolean>(false)
 
   ngOnInit(): void {
     this.getShops();
   }
 
-  getShops(){
-    this._mallsService.getShopsByMallId(1).subscribe(result => {
-      this.listShops = result;
-      console.log("Shops: ", this.listShops)
+  getShops() {
+    this._mallsService.getShopsByMallId(1).subscribe({
+      next: result => {
+        this.listShops = result;
+        console.log("Shops: ", this.listShops)
+      },
+      error: () => {
+        this.finishSearch.set(true);
+      },
+      complete: () => {
+        this.finishSearch.set(true);
+      }
     });
   }
 }
